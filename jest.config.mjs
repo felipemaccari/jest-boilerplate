5a50c8981c3860d41f7f1df56af8a805aa2b8535
Felipe Maccari <felipecesarmaccari@gmail.com>
2023-10-26 00:40:27 -0300

    chore: first commit with initial boilerplate config

---
+++ jest.config.mjs
@@ -0,0 +1,14 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * http
+ */
+
+/** @type {import('jest').Config} */
+const config = {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+};
+
+export default config;
